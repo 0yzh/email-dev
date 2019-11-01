543a6980d814cdf58897a1fef254c93c5fbcfb95
Huy Bui <huyb@huyb-macbookpro2.roam.corp.google.com>
2019-11-01 06:47:04 -0700

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
+/**
+ * import dependencies
+ * @ gulp - task runner
+ * @ premailer - Preflight for HTML and CSS
+ * @ inlinerCSS - CSS inliner
+ */
 const gulp = require('gulp');
 const premailer = require('gulp-premailer');
 const inlineCss = require('gulp-inline-css');
 
+/**
+ * set task
+ * @ default - compiles a final version of index.html from ./stage/ and outputs it into ./build/
+ * @ all string arguments can be modified as needed
+ */
 gulp.task('default', function() {
   return gulp
     .src('./stage/*.html')
